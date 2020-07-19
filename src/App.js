ce7ed5c4aea6cf456aafbce9036b29f5f4c406da
jaredhodgkins <jared.hodgkins@gmail.com>
2020-07-18 20:58:18 -0400

    Section 4 Assignment - My Solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import ValidationComponent from './ValidationComponent/ValidationComponent';
+import CharComponent from './CharComponent/CharComponent';
 
 class App extends Component {
   state = {
@@ -10,7 +12,10 @@ class App extends Component {
       { id: 329, name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
-    showPersons: false
+    showPersons: false,
+    assignment: {
+      text: ''
+    }
   }
 
   deletePersonHandler = (personIndex) => {
@@ -44,6 +49,22 @@ class App extends Component {
     this.setState({showPersons: !doesShow});
   }
 
+  assignmentChangeHandler = (event) => {
+    this.setState({assignment: {
+      text: event.target.value,
+      textLength: event.target.value.length,
+    }})
+  }
+
+  assignmentClickHandler = (charIndex) => {
+    console.log(`index is ${charIndex}`);
+    let assignment = {...this.state.assignment};
+    let textArray = assignment.text.split('');
+    textArray.splice(charIndex,1);
+    assignment.text = textArray.join('');
+    this.setState({assignment: assignment})
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -72,6 +93,22 @@ class App extends Component {
       );
     }
 
+    // assignment specific code
+    const charArray = this.state.assignment.text.split('');
+    const charComponents = (
+      <div>
+        {charArray.map((c, index) => {
+          return <CharComponent
+            char={c}
+            key={index}
+            click={() => this.assignmentClickHandler(index)}
+            />
+        })}
+      </div>
+    );
+
+    // end assignment specific code
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -81,6 +118,14 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Toggle Persons
         </button>
         {persons}
+        <div className="assignment">
+          <input type="text" className="input-field"
+            onChange={(event) => this.assignmentChangeHandler(event)}
+            value={this.state.assignment.text} />
+          <p>{this.state.assignment.text.length}</p>
+          <ValidationComponent textLength={this.state.assignment.text.length} />
+          {charComponents}
+        </div>
       </div>
     );
   }
