649f110b2f71358cbc3e6ba1b5a531134567ceeb
jaredhodgkins <jared.hodgkins@gmail.com>
2020-07-24 10:11:35 -0400

    05_66 Setting Styles Dynamically

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import ValidationComponent from './ValidationComponent/ValidationComponent';
-import CharComponent from './CharComponent/CharComponent';
 
 class App extends Component {
   state = {
@@ -49,31 +47,10 @@ class App extends Component {
     this.setState({showPersons: !doesShow});
   }
 
-  assignmentChangeHandler = (event) => {
-    this.setState({assignment: {
-      text: event.target.value,
-      textLength: event.target.value.length,
-    }})
-  }
-
-  assignmentClickHandler = (charIndex) => {
-    console.log(`index is ${charIndex}`);
-    let assignment = {...this.state.assignment};
-    let textArray = assignment.text.split('');
-    textArray.splice(charIndex,1);
-    assignment.text = textArray.join('');
-    this.setState({assignment: assignment})
-
-    // Instructor's Solution (adapted)
-    // const text = this.state.assignment.text.split('');
-    // text.splice(charIndex, 1);
-    // const updatedText = text.join('');
-    // this.setState({assignment: { text: updatedText }})
-  }
-
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -97,23 +74,9 @@ class App extends Component {
             )})}
         </div>
       );
+
+      style.backgroundColor = 'red';
     }
-
-    // assignment specific code
-    const charArray = this.state.assignment.text.split('');
-    const charComponents = (
-      <div>
-        {charArray.map((c, index) => {
-          return <CharComponent
-            char={c}
-            key={index}
-            click={() => this.assignmentClickHandler(index)}
-            />
-        })}
-      </div>
-    );
-
-    // end assignment specific code
 
     return (
       <div className="App">
@@ -124,14 +87,6 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Toggle Persons
         </button>
         {persons}
-        <div className="assignment">
-          <input type="text" className="input-field"
-            onChange={(event) => this.assignmentChangeHandler(event)}
-            value={this.state.assignment.text} />
-          <p>{this.state.assignment.text.length}</p>
-          <ValidationComponent textLength={this.state.assignment.text.length} />
-          {charComponents}
-        </div>
       </div>
     );
   }
