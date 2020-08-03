b38238ab14a5331abc2f9c211e92d6641f4b8870
jaredhodgkins <jared.hodgkins@gmail.com>
2020-08-03 08:53:24 -0400

    07_91 Component Update Lifecycle (for state Changes)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 // import './Person.css';
@@ -15,18 +15,21 @@ const StyledDiv = styled.div`
   }
 `;
 
-const person = (props) => {
-  console.log('[Person.js] rendering...');
-  return (
-    // <div className="Person">
-    <StyledDiv>
-      <p onClick={props.click}>
-        I'm {props.name} and I am {props.age} years old
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </StyledDiv>
-  );
-};
+class Person extends Component {
+  render() {
+    console.log('[Person.js] rendering...');
 
-export default person;
+    return (
+      // <div className="Person">
+      <StyledDiv>
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old
+        </p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </StyledDiv>
+    );
+  }
+}
+
+export default Person;
